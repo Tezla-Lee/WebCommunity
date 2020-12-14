d6b0cac344c93ea152acc56c4661fcac01b3980e
Tezla-Lee <tezla.lee@gmail.com>
2020-12-14 15:14:52 +0900

    Initialize Project

---
+++ src/main/java/com/leenayoung/WebcommunityApplication.java
@@ -0,0 +1,13 @@
+package com.leenayoung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebcommunityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebcommunityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/leenayoung/WebcommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leenayoung;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebcommunityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
