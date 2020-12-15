81d3d238d94ae9c7221fa76ff0490b086357c20e
FoeverNa <foevna@naver.com>
2020-12-15 20:32:53 +0900

    feature: Add SecurityConfig class

--- src/main/java/com/leenayoung/config/SecurityConfig.java
+++ src/main/java/com/leenayoung/config/SecurityConfig.java
@@ -1,10 +1,23 @@
 package com.leenayoung.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity security) throws Exception {
+        // 사이트간 요청 위조를 비활성화 시킨다
+        security.csrf().disable();
+        security.authorizeRequests().antMatchers("/").permitAll();
+
+        security.formLogin().loginPage("/login").defaultSuccessUrl("/main");
+
+        security.logout().invalidateHttpSession(true).logoutSuccessUrl("/login");
+
+    }
 }
