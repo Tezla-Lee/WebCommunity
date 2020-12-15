2c88c1575cf72644820aa5dda82e9f59495d8b02
Tezla-Lee <tezla.lee@gmail.com>
2020-12-15 14:42:48 +0900

    fix: bug

--- src/test/java/com/leenayoung/service/UserServiceImplTest.java
+++ src/test/java/com/leenayoung/service/UserServiceImplTest.java
@@ -10,8 +10,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 
-import java.util.Date;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 @RunWith(SpringRunner.class)
