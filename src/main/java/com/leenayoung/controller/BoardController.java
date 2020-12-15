6737a38e71d2f6bb900f09739dd57fb1471401bd
Tezla-Lee <tezla.lee@gmail.com>
2020-12-15 14:37:28 +0900

    style: .jsp -> .html

--- src/main/java/com/leenayoung/controller/BoardController.java
+++ src/main/java/com/leenayoung/controller/BoardController.java
@@ -7,6 +7,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 
 @Controller
 public class BoardController {
@@ -48,5 +50,10 @@ public class BoardController {
     public String updateBoard(Board board) {
         boardService.updateBoard(board);
         return "redirect:getBoardList";
+    }
+
+    @RequestMapping("/main")
+    public String login() {
+        return "blogmain_detail";
     }
 }
